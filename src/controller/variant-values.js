56fe37fdeec17fdfd9d262096b3b0b518a48f4ae
rwinanda <mochammadrwinanda@gmail.com>
2025-06-18 14:26:21 +0700

    variant values

--- src/controller/variant-values.js
+++ src/controller/variant-values.js
@@ -1,68 +1,148 @@
 const Database = require('../db/client');
-const fs = require('fs');
-const path = require('path');
+const { uploadPicturesValues } = require('./product-pictures');
 
-exports.createVariantValues = async(req, res) => {
-    try {
+exports.addVariantValues = async (variantId, value, created_at, updated_at, tempVar, respVarValue, files) => {
+    let tempVal = [];    // Looping for value_variant
+    for(let i = 0; i < value.length; i++) {
+        const item = value[i];
+        let value_name, picture_values;
+        
+        const pictureFile = files?.[`picture-value-${i}`];
+        // Condition when using picture value
+        if(item.picture){
+            value_name = item.value_name;
+            const imageName = await uploadPicturesValues(pictureFile)
+            picture_values = imageName
+            
+        } else {
+            value_name = item.value_name;
+            picture_values = null;
+        }
+        
+        const valueQuery = "INSERT INTO variant_values (variant_id, value, picture_values, created_at, updated_at) VALUES ($1, $2, $3, to_timestamp($4,'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ'), to_timestamp($5, 'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ')) RETURNING value_id" 
+        const valueVariant = await Database.db.query(valueQuery, [variantId, value_name, picture_values, created_at, updated_at]);
 
-        // Function for upload image
-        const imageName = await uploadPicturesValues(req);
+        // Push to get respon for insert value_variant
+        respVarValue.push(valueVariant.rows[0]);
+        // Push to array value
+        tempVal.push(valueVariant.rows[0].value_id)
+    }
+    // Push array value to array variant
+    tempVar.push(tempVal);
+    // Clear tempVal after push to tempVar
+    tempVal = []
+}
 
-        const { variant_id, value } = req.body;
-        const created_at = new Date().toISOString();
-        const updated_at = created_at;
+exports.addVariantSKU = async (tempVar, sku_value, created_at, updated_at, respSkuValue) => {
+    let index = 0;
+        // Step 3. Insert SKU
+        for(let i = 0; i < tempVar[0].length; i++){
+            for(let j = 0; j < tempVar[1].length; j++){
+                const { price, weight, stock, sku_name } = sku_value[index];
+                let value1 = tempVar[0][i]; // For value id 1
+                let value2 = tempVar[1][j]; // For Value id 2
 
-        const variant = await Database.db.query("INSERT INTO variant_values (variant_id, value, picture_values, created_at, updated_at) VALUES ($1, $2, $3, to_timestamp($4, 'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ'), to_timestamp($5, 'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ')) RETURNING *", [variant_id, value, imageName, updated_at, created_at]);
+                // Insert to database
+                const skuQuery = "INSERT INTO product_sku (value_id_1, value_id_2, price, stock, weight, sku_name, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, to_timestamp($7,'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ'), to_timestamp($8, 'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ')) RETURNING *" 
+                const skuResult = await Database.db.query(skuQuery, [value1, value2, price, weight, stock, sku_name, created_at, updated_at]);
 
-        return res.status(201).json({
-            status: 201,
-            message: "Successfully created variant values",
-            data: variant.rows
-        })
+                respSkuValue.push(skuResult.rows[0]); // Save to array respon SKU Value
+                index++
+            }
+        }
+}
 
-    } catch (error) {
-        return res.status(500).json({
-            status: 500,
-            message: "Failed",
-            error: error.message
-        });   
+exports.editVariantValues = async(value, updated_at, respVarValue, image) => {
+    for (let i = 0; i < value.length; i++) {
+        const valueId = value[i].value_id;
+        const valueName = value[i].value_name
+        const pictureFile = image?.[`picture_${i}`];
+
+        // console.log("value id ", i, " => ", valueId);
+
+        // Condition when update value name
+        if(valueName){
+            const variantValue = await Database.db.query(
+                `
+                UPDATE variant_values SET value = $1, updated_at = to_timestamp($2, 'YYYY-MM-DD"T"HH24:MI:SS.MSZ') WHERE value_id = $3 RETURNING *;
+                `,
+                [valueName, updated_at, valueId]
+            );
+            
+            // Respon for variant value
+            respVarValue.push(variantValue.rows[0])
+        }
+
+        // Condition when update picture
+        if (pictureFile) {
+            const picValue = await uploadPicturesValues(pictureFile);
+            const pictureQueries = await Database.db.query( `
+            UPDATE variant_values SET picture_values = $1, updated_at = to_timestamp($2, 'YYYY-MM-DD"T"HH24:MI:SS.MSZ') WHERE value_id = $3 RETURNING *;
+            `, [picValue, updated_at, valueId]);
+            
+            // Respon for variant value
+            respVarValue.push(pictureQueries.rows[0])
+        }
     }
 }
 
-async function uploadPicturesValues(req)  {
-    try {
-        let image = req.files.image;
-        let imageName = image.name
+exports.editVariantSKU = async(sku_value, updated_at, respSkuValue) => {
+    for(let i = 0; i < sku_value.length; i++) {
+        console.log("sku value => ", sku_value)
+        const item = sku_value[i];
 
-        const uploadDir = path.join(__dirname, "../", process.env.UPLOAD_DIR || "uploads");
-                
-        // if file uploads directory doesn't exist, create new folder
-        if (!fs.existsSync(uploadDir)) {
-            fs.mkdirSync(uploadDir, { recursive: true });
+        const skuId = item.sku_id;
+        const sku_name = item.sku_name;
+        const price = item.price;
+        const stock = item.stock;
+        const weight = item.weight;
+
+        const fields = []; // field for SET query
+        const values = []; // for value 
+        let index = 1; // index for query
+
+        if (sku_name !== undefined){
+            fields.push(`sku_name = $${index}`);
+            values.push(sku_name);
+            index++;
+        }
+        if (price !== undefined){
+            fields.push(`price = $${index}`);
+            values.push(price);
+            index++
+        }
+        if (stock !== undefined){
+            fields.push(`stock = $${index}`);
+            values.push(stock);
+            index++;
+        }
+        if (weight !== undefined){
+            fields.push(`weight = $${index}`);
+            values.push(weight);
+            index++;
         }
 
-        // Generate a unique filename to prevent overwriting
-        const fileExtension = path.extname(image.name); // .jpg
-        const baseName = path.basename(image.name, fileExtension) // iphone 15_
-        let counter = 1;
-        
-        // upload file to path
-        let uploadPath = path.join(uploadDir, imageName);
+        // Update for updated_at
+        fields.push(
+        `updated_at = to_timestamp($${index}, 'YYYY-MM-DD"T"HH24:MI:SS.MSZ')`
+        );
+        values.push(updated_at);
+        index++;
 
-        // Check for duplicate file name and rename it
-        while(fs.existsSync(uploadPath)) {
-            imageName = `${baseName}_${counter}${fileExtension}`;
-            uploadPath = path.join(uploadDir, imageName);
-            counter++;
-        }
+        values.push(skuId)
 
-        // Move the file (mv requires a callback function)
-        await image.mv(uploadPath);
 
-        return imageName; // return the new filename
+        console.log("field => ", fields)
+        console.log("index => ", index)
+        console.log("values => ", values)
 
-    } catch (error) {
-        console.error("Error uploading file:", error.message);
-        throw new Error("File upload failed");
+        const skuQuery = await Database.db.query(`
+            UPDATE product_sku SET ${fields.join(", ")}
+            WHERE sku_id = $${index} RETURNING *;
+        `, values)
+
+        respSkuValue.push(skuQuery.rows[0])
+
+        console.log("query => ", skuQuery.rows);
     }
 }
--- src/routes/variant-values.js
+++ src/routes/variant-values.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const variantValueController = require('../controller/variant-values');
 const authMiddleware = require('../middleware/auth');
 
-router.post('/variant_values', variantValueController.createVariantValues);
+// router.post('/variant_values', variantValueController.createVariantValues);
 router.get('/variant_values', authMiddleware.checkAuth);
 
 module.exports = router;
