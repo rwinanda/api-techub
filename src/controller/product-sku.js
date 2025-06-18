94443260c4e60c4c0c9455563ecb05df749b2ef6
rwinanda <mochammadrwinanda@gmail.com>
2025-06-18 14:22:58 +0700

    product sku

--- src/controller/product-sku.js
+++ src/controller/product-sku.js
@@ -1,13 +1,13 @@
 const Database = require('../db/client');
 
 // Create Product SKU
-exports.createProductSku = async(req, res, next) => {
+exports.createProductSku = async(req, res) => {
     try {
-        const { value_id, sku_name, price, stock } = req.body;
+        const { product_id, value_id_1, value_id_2, sku_name, price, stock, weight } = req.body;
         const createdAt = new Date().toISOString();
         const updatedAt = createdAt;
         const productSku = await Database.db.query(
-            "INSERT INTO product_sku (value_id, sku_name, price, stock, created_at, updated_at) VALUES ($1, $2, $3, $4,  to_timestamp($5, 'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ'), to_timestamp($6, 'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ')) RETURNING *", [value_id, sku_name, price, stock, updatedAt, createdAt]
+            "INSERT INTO product_sku (product_id, value_id_1, value_id_2, sku_name, price, stock, weight, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7, to_timestamp($8, 'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ'), to_timestamp($9, 'YYYY-MM-DD\"T\"HH24:MI:SS.MSZ')) RETURNING *", [product_id, value_id_1, value_id_2, sku_name, price, stock, weight, updatedAt, createdAt]
         );
 
         return res.status(201).json({
@@ -25,7 +25,7 @@ exports.createProductSku = async(req, res, next) => {
 }
 
 // Get All Product SKU
-exports.getProductSku = async(req, res, next) => {
+exports.getProductSku = async(req, res) => {
     try {
         // console.log("Fetching all product_sku data...");
         const products = await Database.db.query(
@@ -49,7 +49,7 @@ exports.getProductSku = async(req, res, next) => {
 // Get data product SKU by id
 exports.getProductSkuById = async (req, res) => {
     try {
-        const products = `
+        const query = `
         SELECT pr.name_product, ps.price as price_sku, ps.stock as stock_sku, vv.value
         FROM products pr
         LEFT JOIN product_variants pv ON pr.product_id = pv.product_id
@@ -58,9 +58,12 @@ exports.getProductSkuById = async (req, res) => {
         WHERE pr.product_id = 1
         `;
 
-        return res.status(201).json({
-            status:201,
-            message: "Success Get Data Product SKU by ID"
+        const products = await Database.db.query(query)
+
+        return res.status(200).json({
+            status:200,
+            message: "Success Get Data Product SKU by ID",
+            data: products.rows
         })
     } catch (error) {
         return res.status(500).json({
