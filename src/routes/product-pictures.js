260fef38bbd8cd71115a27a27b6d23e33fb1bb97
rwinanda <mochammadrwinanda@gmail.com>
2025-06-18 14:25:20 +0700

    route for product pictures

--- src/routes/product-pictures.js
+++ src/routes/product-pictures.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const productPicturesController = require('../controller/product-pictures');
-const uploadFileMiddleware = require('../middleware/upload-file');
-const authMiddleware = require('../middleware/auth');
+// const uploadFileMiddleware = require('../middleware/upload-file');
+// const authMiddleware = require('../middleware/auth');
 
-router.post("/upload", uploadFileMiddleware.filesUploadImages, productPicturesController.uploadPicturesProduct)
+// router.post("/upload",  authMiddleware.checkAuth, uploadFileMiddleware.filesUploadImages, productPicturesController.uploadPicturesProduct);
+// router.put("/upload/update/:pictureId", productPicturesController.updatePictures);
+router.delete("/upload/delete/:pictureId", productPicturesController.deletePictures);
 
 module.exports = router;
