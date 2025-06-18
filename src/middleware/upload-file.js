8b5cd83c0519421552712beb943636804ed319a8
rwinanda <mochammadrwinanda@gmail.com>
2025-06-18 14:24:58 +0700

    extension upload file

--- src/middleware/upload-file.js
+++ src/middleware/upload-file.js
@@ -1,72 +1,20 @@
-const fileUpload = require('express-fileupload');
+// const fileUpload = require('express-fileupload');
 const path = require('path');
-const MB = 5 * 1024 * 1024; // 5 MB
+const MB = 20 * 1024 * 1024; // 20 MB
 
-exports.uploadFile = async(req, res) => {
-    fileUpload({
-        createParentPath: true
-    });
-};
-
-exports.filesUploadImages = async(req, res, next) => {
-    // Condition if file doesnt exist
-    
-    if (!req.files) {
-        return res.status(400).json({
-            status: 400,
-            error: "No File Uploaded",
-        });
-    }
-    
-    let image = req.files.image;
-    // console.log(image);
-
+exports.filesUploadImages = async(image, req, res) => {
     // Allowed extension files
+     // Validasi ekstensi
     const allowedExtensions = /jpeg|jpg|png/;
-    const extName = path.extname(image.name).toLowerCase();
-
-    // Condition if extension files is not allowed
-    if(!allowedExtensions.test(extName)) {
-        return res.status(400).json({
-            status: 400,
-            message: "Only .jpg, ,jpeg and .png files are allowed "
-        });
+    const extName = path.extname(image).toLowerCase();
+    if (!allowedExtensions.test(extName)) {
+        return res.status(400).json({ message: "Only .jpg, .jpeg and .png allowed" });
     }
 
-    // Condition if file size limit
-    if(image > MB){
-        return res.status(400).json({
-            status: 400,
-            message: "File must be less than 5MB"
-        });
+    // Validasi size
+    if (image.size > MB) {
+        return res.status(400).json({ message: "File must be less than 20MB" });
     }
-    next();
+    
+    // next();
 }
-
-// exports.filesPayloadExist = async(req, res, next) => {
-//     if (!req.files) {
-//         return res.status(400).json({
-//             status: 400,
-//             error: "No File Uploaded",
-//         });
-//     }
-//     next();
-// }
-
-// exports.filesSizeLimit = async(req, res, next) => {
-//     const files = req.files;
-//     const fileOverLimit = [];
-
-//     // Condition if file over limit
-//     Object.keys(files).forEach(key => {
-//         if (files[key].size > FILE_SIZE_LIMIT) {
-//             fileOverLimit.push(files[key].name);
-//         }
-//     });
-
-//     if (fileOverLimit.length) {
-//         console.log(fileOverLimit);
-//     }
-
-//     next();
-// }
