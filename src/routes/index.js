11f31296f3e5fab5e49a9f51431a9b89aecb6a70
Mochammad Winanda <mochammadrwinanda@gmail.com>
2026-09-21 14:35:53 +0700

    Add payment and order routes to API for enhanced functionality

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,17 @@ import userRouter from './users.js';
 import categoryRouter from './categories.js';
 import picsRouter from './pictures.js';
 import cartRouter from './cart.js';
+import paymentRouter from './payment.js';
+import orderRouter from './order.js';
 
 const apiRoutes = [
     { path: "/api/v1/product", route: productRouter },
     { path: "/api/v1/auth", route: userRouter },
     { path: "/api/v1/category", route: categoryRouter },
     { path: "/api/v1/upload", route: picsRouter },
-    { path: "/api/v1/cart", route: cartRouter }
+    { path: "/api/v1/cart", route: cartRouter },
+    { path: "/api/v1/payment", route: paymentRouter },
+    { path: "/api/v1/order", route: orderRouter }
 ]
 
 export default apiRoutes;
